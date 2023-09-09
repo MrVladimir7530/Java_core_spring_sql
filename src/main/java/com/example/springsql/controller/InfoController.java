0238e4e63fa775336c18d7cd176f4a84be4a0a05
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-09-09 10:41:29 +0300

    Добавил логирование 3 шаг

--- src/main/java/com/example/springsql/controller/InfoController.java
+++ src/main/java/com/example/springsql/controller/InfoController.java
@@ -3,9 +3,11 @@ package com.example.springsql.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/info")
 public class InfoController {
     @Value("${server.port}")
     private Integer port;
